c507cb2f7024b97415170a2bed4823ef25951f77
Enes Yıldırım <enes.yildirim@std.yeditepe.edu.tr>
2025-07-09 13:24:32 +0300

    learnpytorch.io part:6 homework

--- train.py
+++ train.py
@@ -5,25 +5,29 @@ import data_setup, engine, model_builder, utils
 from torchvision import transforms
 
 # Setup hyperparameters
-NUM_EPOCHS = 5
-BATCH_SIZE = 32
-HIDDEN_UNITS = 10
-LEARNING_RATE = 0.001
 
-# Setup directories
-train_dir = "data/pizza_steak_sushi/train"
-test_dir = "data/pizza_steak_sushi/test"
-
-# Setup target device
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Create transforms
-data_transform = transforms.Compose([
-  transforms.Resize((64, 64)),
-  transforms.ToTensor()
-])
 
 if __name__ == '__main__':
+
+  NUM_EPOCHS = 5
+  BATCH_SIZE = 32
+  HIDDEN_UNITS = 10
+  LEARNING_RATE = 0.001
+
+  # Setup directories
+  train_dir = "data/pizza_steak_sushi/train"
+  test_dir = "data/pizza_steak_sushi/test"
+
+  # Setup target device
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+
+  # Create transforms
+  data_transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+  ])
+
+
   # Create DataLoaders with help from data_setup.py
   train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
       train_dir=train_dir,
